33c453558fd9ae64bb78f8cece0752be86916cca
ebrahim ghaemi <e.ghaemi@vada.ir>
2017-03-27 10:15:52 +0430

    add param and log

--- functions.php
+++ functions.php
@@ -1,12 +1,10 @@
 <?php
 
                 
-function notification($msisdn, $trans_id, $trans_status, $datetime, $cahnnel, $shortcode, $keyword, $charge_code, $billed_price_point, $event_type, $validity, $next_renewal_date, $status)
+function notification($param)
 {
-    $line = $msisdn . "|" . $trans_id . "|" . $trans_status  . "|" .  $datetime  . "|" .  $cahnnel  . "|" .
-            $shortcode  . "|" . $keyword  . "|" . $charge_code  . "|" . $billed_price_point  . "|" .
-            $event_type  . "|" . $validity  . "|" . $next_renewal_date  . "|" . $status;
-    file_put_contents(__DIR__ . "/logs.log", print_r($line . "\n", true), FILE_APPEND);
+
+    file_put_contents(__DIR__ . "/logs.log", print_r($param . "\n", true), FILE_APPEND);
 
     return 'true';
 }
